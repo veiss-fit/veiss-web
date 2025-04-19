8061c05d43192b0c29157ceb3548a4aa32623844
Anusha Chitranshi <chitraaa@mail.uc.edu>
2025-04-19 14:49:12 -0400

    clean up

--- src/app/[locale]/(web)/page.tsx
+++ src/app/[locale]/(web)/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-// import PreviewBanner from "./components/PreviewBanner/PreviewBanner";
-// import OurPrograms from "./components/OurPrograms/OurPrograms";
 import SubscribeBanner from "./components/SubscribeBanner/SubscribeBanner";
 // import ShowcaseVideo from "./components/ShowcaseVideo/ShowcaseVideo";
-import ProgramsList from "./components/ProgramsList/ProgramsList";
 import DescriptiveTabs from "./components/DescriptiveTabs/DescriptiveTabs";
-// import TeamNumbers from "./components/TeamNumbers/TeamNumbers";
+import TeamNumbers from "./components/TeamNumbers/TeamNumbers";
 import StartExcercising from "./components/StartExcercising/StartExcercising";
 // import Testemonial from "./components/Testemonial/Testemonial";
-// import BlogSlider from "./components/BlogSlider/BlogSlider";
 // import PricingList from "./components/PricingList/PricingList";
 import OurTeam from "./components/OurTeam/OurTeam";
 import HeroSlider from "./components/HeroSlider/HeroSlider";
@@ -18,16 +14,11 @@ export default function Home() {
   return (
     <div>
       <HeroSlider />
-      {/* <ProgramsList /> */}
-      {/* <PreviewBanner /> */}
-      {/* <OurPrograms /> */}
       <DescriptiveTabs />
       <SubscribeBanner />
       {/* <PricingList /> */}
-      {/* <PreviewBanner /> */}
       {/* <Testemonial /> */}
-      
-      {/* <TeamNumbers /> */}
+      <TeamNumbers />
       <OurTeam />
       {/* <ShowcaseVideo /> */}
       <StartExcercising />
